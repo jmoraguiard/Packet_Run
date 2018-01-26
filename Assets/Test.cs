9b452d89d32fe92016119df44bc903395584d07b
Joan Mora <joan@inflight-vr.com>
2018-01-26 22:34:44 +0100

    Create scene to test PlayMaker

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void RandRotCube()
+	{
+		float rotAmount = Random.Range (-40, 40);
+		Debug.Log (rotAmount);
+
+		transform.rotation = Quaternion.Euler (0, rotAmount, 0);
+	}
+}
