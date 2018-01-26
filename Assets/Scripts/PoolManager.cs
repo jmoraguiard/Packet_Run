26dc7d5594f40782e638074b205436389deda51d
Ennio Casas Puglielli <theepsi@gmail.com>
2018-01-26 22:49:10 +0100

    Create Pool for Cables

---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour {
+
+	public GameObject PooledObject;
+	public int Amount;
+
+	private List<GameObject> _pooledObjects;
+
+	void Awake()
+	{
+		_pooledObjects = new List<GameObject> ();
+
+		GameObject parent = new GameObject("Pool of " + PooledObject.name);
+
+		for (int i = 0; i < Amount; ++i)
+		{
+			GameObject go = Instantiate(PooledObject);
+			go.SetActive(false);
+			go.transform.parent = parent.transform;
+			_pooledObjects.Add(go);
+		}
+	}
+
+	public GameObject GetPooledObject () {
+		for (int i = 0; i < _pooledObjects.Count; ++i) {
+			if (!_pooledObjects [i].activeInHierarchy) {
+				return _pooledObjects [i];
+			}
+		}
+		GameObject parent = new GameObject("Pool of " + PooledObject.name);
+
+		GameObject go = Instantiate(PooledObject);
+		go.SetActive(false);
+		go.transform.parent = parent.transform;
+		_pooledObjects.Add(go);
+
+		return go;
+	}
+}
