15b2531ed1ced463a82b21e19c43264646272b57
Joan Mora <joan@inflight-vr.com>
2018-01-27 20:38:52 +0100

    Fix X Player Positions to Level Manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -145,15 +145,13 @@ public class LevelManager : MonoBehaviour {
 
     public void setPlayersXPositions(float[] xPositions) {
         bool shouldGoFast = true;
-		Debug.Log ("Received positions:");
         for (int i = 0; i < xPositions.Length; ++i) {
             shouldGoFast = shouldGoFast && (xPositions[i] > _sizeOfVisibleCable *_cableOffset / 2);
-			Debug.Log (xPositions[i] + " should go fast? " +shouldGoFast.ToString());
         }
         bool shouldGoSlow = true;
         for (int i = 0; i < xPositions.Length; ++i)
         {
-            shouldGoFast = shouldGoFast && (xPositions[i] < _sizeOfVisibleCable * _cableOffset / 2);
+			shouldGoSlow = shouldGoSlow && (xPositions[i] < _sizeOfVisibleCable * _cableOffset / 2);
         }
         if (shouldGoFast) {
             _speedMultiplier = 1.25f;
