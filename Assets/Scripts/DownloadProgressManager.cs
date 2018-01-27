0c973ed2a14a92d61f8f8ceb0d6edf21007fcf03
Ennio Casas Puglielli <theepsi@gmail.com>
2018-01-27 17:46:30 +0100

    Add Timer to DownloadProgress prefab

--- Assets/Scripts/DownloadProgressManager.cs
+++ Assets/Scripts/DownloadProgressManager.cs
@@ -8,25 +8,35 @@ public class DownloadProgressManager : MonoBehaviour {
 
     public Image Progress;
 
+    public Text TimeCode;
+
     public float UpdateRatio = .01f;
 
-    public Action OnProgressEnds;
+    public Action<float> OnProgressEnds;
+
+    private float timer = 0;
 
 	// Use this for initialization
 	void Start () {
         Progress.fillAmount = .0f;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
         if (LevelManager.Instance.IsRunning()) {
             if (Progress.fillAmount < 1)
             {
+                timer += Time.deltaTime;
                 Progress.fillAmount += UpdateRatio * Time.deltaTime; // TODO: Add velocity multiplier
+
+                int minutes = Mathf.FloorToInt(timer / 60F);
+                int seconds = Mathf.FloorToInt(timer - minutes * 60);
+
+                TimeCode.text = "Time: " + string.Format("{0:00}:{1:00}", minutes, seconds);
             }
             else
             {
-                if (OnProgressEnds != null) OnProgressEnds();
+                if (OnProgressEnds != null) OnProgressEnds(timer);
             }
         }
 	}
